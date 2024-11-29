467fc753be5b006adc5b84385ea2d11318d86ccb
Edwin HC <edwinrhc@gmail.com>
2024-11-29 18:13:38 -0500

    update

--- src/main/java/org/st/gob/pe/sifonavic8/Sifonavic8Application.java
+++ src/main/java/org/st/gob/pe/sifonavic8/Sifonavic8Application.java
@@ -3,10 +3,17 @@ package org.st.gob.pe.sifonavic8;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("org.st.gob.pe.sifonavic8.mapper")
-public class Sifonavic8Application {
+public class Sifonavic8Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Sifonavic8Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Sifonavic8Application.class, args);
