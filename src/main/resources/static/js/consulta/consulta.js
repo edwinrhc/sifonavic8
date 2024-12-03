a98405c7f98da2310ab26c4f7dab034575c1147f
Edwin HC <edwinrhc@gmail.com>
2024-12-03 10:19:17 -0500

    update

--- src/main/resources/static/js/consulta/consulta.js
+++ src/main/resources/static/js/consulta/consulta.js
@@ -1,5 +1,31 @@
+function validarCampos() {
+    var tipoDoc = document.getElementById("tipoDocumento").value;
+    var numDoc = document.getElementById("numDocumento").value;
+
+    // Obtener los valores de los campos
+    var tipoDocValue = tipoDoc.trim();
+    var numDocValue = numDoc.trim();
+
+    // Verificar si ambos campos tienen un valor
+    if (tipoDocValue !== "" && numDocValue !== "") {
+        // Si ambos campos tienen un valor, habilitar el botón y restablecer el estilo
+        document.getElementById("validarBoton").disabled = false;
+        document.getElementById("validarBoton").style.backgroundColor = ""; // Restablecer el color de fondo
+        document.getElementById("validarBoton").style.color = ""; // Restablecer el color del texto
+        document.getElementById("validarBoton").style.cursor = ""; // Restablecer el cursor
+    } else {
+        // Si al menos uno de los campos está vacío, desactivar el botón y aplicar el estilo de bloqueado
+        document.getElementById("validarBoton").disabled = true;
+        document.getElementById("validarBoton").style.backgroundColor = "gray"; // Cambiar el color de fondo a gris
+        document.getElementById("validarBoton").style.color = "white"; // Cambiar el color del texto a blanco
+        document.getElementById("validarBoton").style.cursor = "not-allowed"; // Cambiar el cursor a no permitido
+    }
+}
+
+
 $(document).ready(function () {
     console.log('Context Path:', contextPath);
+    $('#resultsTable').addClass('hidden');
 
     // Mapeo de tipo de documento
     const tipoDocumentoMap = {
@@ -44,24 +70,33 @@ $(document).ready(function () {
                 // Limpia la tabla antes de actualizarla
                 $('#resultsTableBody').empty();
 
-                // Itera sobre los resultados y crea filas dinámicas
-                data.consultaPersona.forEach(persona => {
-                    const row = `
-                        <tr>
-                            <td class="border border-gray-300 px-4 py-2">${tipoDocumentoMap[persona.tipoDocumento]}</td>
-                            <td class="border border-gray-300 px-4 py-2">${persona.numDocumento}</td>
-                            <td class="border border-gray-300 px-4 py-2">${persona.apePaterno}</td>
-                            <td class="border border-gray-300 px-4 py-2">${persona.apeMaterno}</td>
-                            <td class="border border-gray-300 px-4 py-2">${persona.nombres}</td>
-                            <td class="border border-gray-300 px-4 py-2">${formatDate(persona.fechaNacimiento)}</td>
-                            <td class="border border-gray-300 px-4 py-2">${sexoMap[persona.sexo]}</td>
-                            <td class="border border-gray-300 px-4 py-2">${persona.estadoFallecido ? 'Sí' : 'No'}</td>
-                            <td class="border border-gray-300 px-4 py-2">${persona.fechaFallecido ? formatDate(persona.fechaFallecido) : 'Sin datos'}</td>
-                        </tr>
-                    `;
-                    $('#resultsTableBody').append(row);
-                });
-                console.log(data.consultaPersona);
+                if(data.consultaPersona && data.consultaPersona.length > 0) {
+                    // Si hay resultados, muestra la tabla y oculta el mensaje
+                    $('#resultsTable').removeClass('hidden');
+                    $('#noResultsMessage').addClass('hidden');
+
+                    // Itera sobre los resultados y crea filas dinámicas
+                    data.consultaPersona.forEach(persona => {
+                        const row = `
+                            <tr>
+                                <td class="border border-gray-300 px-4 py-2">${tipoDocumentoMap[persona.tipoDocumento]}</td>
+                                <td class="border border-gray-300 px-4 py-2">${persona.numDocumento}</td>
+                                <td class="border border-gray-300 px-4 py-2">${persona.apePaterno}</td>
+                                <td class="border border-gray-300 px-4 py-2">${persona.apeMaterno}</td>
+                                <td class="border border-gray-300 px-4 py-2">${persona.nombres}</td>
+                                <td class="border border-gray-300 px-4 py-2">${formatDate(persona.fechaNacimiento)}</td>
+                                <td class="border border-gray-300 px-4 py-2">${sexoMap[persona.sexo]}</td>
+                                <td class="border border-gray-300 px-4 py-2">${persona.estadoFallecido ? 'Sí' : 'No'}</td>
+                                <td class="border border-gray-300 px-4 py-2">${persona.fechaFallecido ? formatDate(persona.fechaFallecido) : 'Sin datos'}</td>
+                            </tr>
+                        `;
+                        $('#resultsTableBody').append(row);
+                    });
+                } else {
+                    // Si no hay resultados, muestra el mensaje y oculta la tabla
+                    $('#resultsTable').addClass('hidden');
+                    $('#noResultsMessage').removeClass('hidden')
+                }
             },
             error: function (xhr, status, error) {
                 console.error('Error:', error);
@@ -69,28 +104,4 @@ $(document).ready(function () {
             }
         });
     });
-
-    function validarCampos() {
-        var tipoDoc = document.getElementById("tipoDocumento").value;
-        var numDoc = document.getElementById("numDocumento").value;
-
-        // Obtener los valores de los campos
-        var tipoDocValue = tipoDoc.trim();
-        var numDocValue = numDoc.trim();
-
-        // Verificar si ambos campos tienen un valor
-        if (tipoDocValue !== "" && numDocValue !== "") {
-            // Si ambos campos tienen un valor, habilitar el botón y restablecer el estilo
-            document.getElementById("validarBoton").disabled = false;
-            document.getElementById("validarBoton").style.backgroundColor = ""; // Restablecer el color de fondo
-            document.getElementById("validarBoton").style.color = ""; // Restablecer el color del texto
-            document.getElementById("validarBoton").style.cursor = ""; // Restablecer el cursor
-        } else {
-            // Si al menos uno de los campos está vacío, desactivar el botón y aplicar el estilo de bloqueado
-            document.getElementById("validarBoton").disabled = true;
-            document.getElementById("validarBoton").style.backgroundColor = "gray"; // Cambiar el color de fondo a gris
-            document.getElementById("validarBoton").style.color = "white"; // Cambiar el color del texto a blanco
-            document.getElementById("validarBoton").style.cursor = "not-allowed"; // Cambiar el cursor a no permitido
-        }
-    }
 });
