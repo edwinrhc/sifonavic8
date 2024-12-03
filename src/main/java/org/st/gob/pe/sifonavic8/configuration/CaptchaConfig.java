9f3d5ebdfd00b9a01388a6ea9eac0ab8acd2e477
Edwin HC <edwinrhc@gmail.com>
2024-12-03 17:05:10 -0500

    delete codigo basura

--- src/main/java/org/st/gob/pe/sifonavic8/configuration/CaptchaConfig.java
+++ src/main/java/org/st/gob/pe/sifonavic8/configuration/CaptchaConfig.java
@@ -10,19 +10,7 @@ import java.util.Properties;
 @Configuration
 public class CaptchaConfig {
 
-  /*  @Bean
-    public DefaultKaptcha getKaptchaBean() {
-        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
-        Properties properties = new Properties();
-        properties.setProperty("kaptcha.border", "no");
-        properties.setProperty("kaptcha.textproducer.font.color", "black");
-        properties.setProperty("kaptcha.textproducer.char.space", "5");
-        properties.setProperty("kaptcha.image.width", "200");
-        properties.setProperty("kaptcha.image.height", "50");
-        properties.setProperty("kaptcha.textproducer.font.size", "40");
-        defaultKaptcha.setConfig(new Config(properties));
-        return defaultKaptcha;
-    }*/
+
   @Bean
   public DefaultKaptcha captchaProducer() {
       DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
